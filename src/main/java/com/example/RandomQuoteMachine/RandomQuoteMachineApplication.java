232775ad2945b0db7f1ef9044aa4626ff185f548
Jalisa Hewitt <Jalisa@Jalisas-MacBook-Pro.local>
2018-08-29 18:32:04 -0400

    first commit

---
+++ src/main/java/com/example/RandomQuoteMachine/RandomQuoteMachineApplication.java
@@ -0,0 +1,12 @@
+package com.example.RandomQuoteMachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomQuoteMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomQuoteMachineApplication.class, args);
+	}
+}
